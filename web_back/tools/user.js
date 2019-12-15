96e3e536d5dfb3b77287487bc87ed634548dfe31
cxh <564668501@qq.com>
2019-12-15 17:04:00 +0800

    完善了登录页面、使用user.js管理接口、实现了用户退出功能

---
+++ web_back/tools/user.js
@@ -0,0 +1,37 @@
+// 使用user.js对项目中与用户操作有关的请求功能进行保存
+var user = {
+    // 设置login方法，用来进行登录请求操作
+    login: function (options) {
+        $.ajax({
+            type: 'POST',
+            url: LOGIN, //将地址进行特殊处理
+            data: {
+                user_name: options.data.user_name,
+                password: options.data.password
+            },
+            success: function (res) {
+                // console.log(res);
+                // 4.根据响应状态进行判断
+                if (res.code === 200) {
+                    options.success();
+                } else {
+                    options.fail();
+                }
+            }
+        });
+    },
+    // 设置logout方法，用来进行退出操作
+    logout: function (options) {
+        $.ajax({
+            type: 'POST',
+            url: LOGOUT,
+            success: function (res) {
+                if (res.code === 200) {
+                    options.success();
+                } else {
+                    options.fail();
+                }
+            }
+        })
+    }
+};
